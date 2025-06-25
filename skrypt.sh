948eb389b4ad567863d16843363185cc26739580
Kordight <164764949+Kordight@users.noreply.github.com>
2025-06-25 21:24:18 +0200

    Add --error and -e flags to create error folders

--- skrypt.sh
+++ skrypt.sh
@@ -1,4 +1,7 @@
-elif [[ $1 == "--init" ]]; then
-  git clone <URL_DO_REPO> cloned_repo
-  export PATH=$PATH:$(pwd)/cloned_repo
+elif [[ $1 == "--error" || $1 == "-e" ]]; then
+  num=${2:-100}
+  for i in $(seq 1 $num); do
+    mkdir -p error$i
+    echo "error$i.txt created by skrypt.sh on $(date)" > error$i/error$i.txt
+  done
 fi
