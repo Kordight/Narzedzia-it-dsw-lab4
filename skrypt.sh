68acd77720a4dd5d2a4bd276445828107a8c724f
Kordight <164764949+Kordight@users.noreply.github.com>
2025-06-25 21:22:52 +0200

    Add short flags -d, -l, -h

--- skrypt.sh
+++ skrypt.sh
@@ -1,5 +1,10 @@
-elif [[ $1 == "--help" ]]; then
-  echo "--date         Print current date"
-  echo "--logs [n]     Generate n log files (default 100)"
-  echo "--help         Show help"
-fi
+case $1 in
+  --date|-d) date ;;
+  --logs|-l) num=${2:-100}
+             for i in $(seq 1 $num); do
+               echo "log$i.txt created by skrypt.sh on $(date)" > log$i.txt
+             done ;;
+  --help|-h) echo "--date | -d      Print current date"
+             echo "--logs | -l N    Generate N log files (default 100)"
+             echo "--help | -h      Show help" ;;
+esac
