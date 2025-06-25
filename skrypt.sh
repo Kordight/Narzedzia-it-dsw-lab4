6a680bd8b2589742c2d29f15cb36e07669f65566
Kordight <164764949+Kordight@users.noreply.github.com>
2025-06-25 21:19:36 +0200

    Add --logs flag to generate 100 log files

--- skrypt.sh
+++ skrypt.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-
-if [[ $1 == "--date" ]]; then
-  date
+elif [[ $1 == "--logs" ]]; then
+  for i in {1..100}; do
+    echo "log$i.txt created by skrypt.sh on $(date)" > log$i.txt
+  done
 fi
