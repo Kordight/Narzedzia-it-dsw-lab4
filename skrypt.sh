29db74568ab271fbd7a853a5cdbaa8aeb1137a75
Kordight <164764949+Kordight@users.noreply.github.com>
2025-06-25 21:23:22 +0200

    Add --init to clone repo and update PATH

--- skrypt.sh
+++ skrypt.sh
@@ -1,10 +1,4 @@
-case $1 in
-  --date|-d) date ;;
-  --logs|-l) num=${2:-100}
-             for i in $(seq 1 $num); do
-               echo "log$i.txt created by skrypt.sh on $(date)" > log$i.txt
-             done ;;
-  --help|-h) echo "--date | -d      Print current date"
-             echo "--logs | -l N    Generate N log files (default 100)"
-             echo "--help | -h      Show help" ;;
-esac
+elif [[ $1 == "--init" ]]; then
+  git clone <URL_DO_REPO> cloned_repo
+  export PATH=$PATH:$(pwd)/cloned_repo
+fi
